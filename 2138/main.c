a5cf0b8b359e029af5b8be03b8c03f381d465130
236559 Kirsz Nikodem <236559@edu.p.lodz.pl>
2022-05-18 13:00:29 +0200

    Accel ball mover

--- 2138/main.c
+++ 2138/main.c
@@ -23,37 +23,43 @@
 
 #define PROC1_STACK_SIZE 1024
 #define PROC2_STACK_SIZE 1024
-#define INIT_STACK_SIZE  400
+#define ACC_X_CTRL_STACK_SIZE 1024
+#define ACC_Y_CTRL_STACK_SIZE 1024
+#define INIT_STACK_SIZE 400
 
 static tU8 proc1Stack[PROC1_STACK_SIZE];
 static tU8 proc2Stack[PROC2_STACK_SIZE];
+static tU8 accXCtrlStack[ACC_X_CTRL_STACK_SIZE];
 static tU8 initStack[INIT_STACK_SIZE];
 static tU8 pid1;
 static tU8 pid2;
+static tU8 pidAccXCtrl;
 
 static tU8 lcdHeight = 130;
 static tU8 lcdWidth = 130;
 
-struct Ball {
-    int xPos;
-    int yPos;
-    int radius;
-    int speed;
+struct Ball
+{
+  int xPos;
+  int yPos;
+  int radius;
+  int speed;
 };
 
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right
+enum Direction
+{
+  Up,
+  Down,
+  Left,
+  Right
 };
 
 extern void initKeyProc(void);
 
-static void initProc(void* arg);
+static void initProc(void *arg);
 static void drawWelcome(void);
-static void overdrawBall(struct Ball* ball, int color);
-static void moveBall(struct Ball* ball, tU8 joyDirection);
+static void overdrawBall(struct Ball *ball, int color);
+static void moveBall(struct Ball *ball, tU8 joyDirection);
 static void playWithTheBall(void);
 
 static tS8 direction = KEY_NOTHING;
@@ -65,25 +71,25 @@ volatile tU8 rgbSpeed = 10;
 /******************************************************************************
 ** Function name:		udelay
 **
-** Descriptions:		
+** Descriptions:
 **
 ** parameters:			delay length
 ** Returned value:		None
-** 
+**
 ******************************************************************************/
-void udelay( unsigned int delayInUs )
+void udelay(unsigned int delayInUs)
 {
   /*
    * setup timer #1 for delay
    */
-  T1TCR = 0x02;          //stop and reset timer
-  T1PR  = 0x00;          //set prescaler to zero
-  T1MR0 = (((long)delayInUs-1) * (long)CORE_FREQ/1000) / 1000;
-  T1IR  = 0xff;          //reset all interrrupt flags
-  T1MCR = 0x04;          //stop timer on match
-  T1TCR = 0x01;          //start timer
-  
-  //wait until delay time has elapsed
+  T1TCR = 0x02; // stop and reset timer
+  T1PR = 0x00;  // set prescaler to zero
+  T1MR0 = (((long)delayInUs - 1) * (long)CORE_FREQ / 1000) / 1000;
+  T1IR = 0xff;  // reset all interrrupt flags
+  T1MCR = 0x04; // stop timer on match
+  T1TCR = 0x01; // start timer
+
+  // wait until delay time has elapsed
   while (T1TCR & 0x01)
     ;
 }
@@ -91,11 +97,10 @@ void udelay( unsigned int delayInUs )
 /*****************************************************************************
  *
  * Description:
- *    The first function to execute 
+ *    The first function to execute
  *
  ****************************************************************************/
-int
-main(void)
+int main(void)
 {
   tU8 error;
   tU8 pid;
@@ -103,7 +108,7 @@ main(void)
   osInit();
   osCreateProcess(initProc, initStack, INIT_STACK_SIZE, &pid, 1, NULL, &error);
   osStartProcess(pid, &error);
-  
+
   osStart();
   return 0;
 }
@@ -111,51 +116,52 @@ main(void)
 /*****************************************************************************
  *
  * Description:
- *    A process entry function 
+ *    A process entry function
  *
  * Params:
- *    [in] arg - This parameter is not used in this application. 
+ *    [in] arg - This parameter is not used in this application.
  *
  ****************************************************************************/
 static void
-proc1(void* arg)
+proc1(void *arg)
 {
   tU8 pca9532Present = FALSE;
-    
-    osSleep(50);
 
-    //check if connection with PCA9532
-    pca9532Present = pca9532Init();
-    tU8 pin = 0;
-  
-	for(;;)
-	{
-	    if (TRUE == pca9532Present)
-	    {
-            setPca9532Pin(pin, 0);
-            udelay(400000);
-            setPca9532Pin(pin, 1);
-            pin = (pin + 1) % 16;
-        }
+  osSleep(50);
+
+  // check if connection with PCA9532
+  pca9532Present = pca9532Init();
+  tU8 pin = 0;
+
+  for (;;)
+  {
+    if (TRUE == pca9532Present)
+    {
+      setPca9532Pin(pin, 0);
+      udelay(400000);
+      setPca9532Pin(pin, 1);
+      pin = (pin + 1) % 16;
     }
+  }
 }
-
 
 typedef enum _ButtonError_t
 {
-  BUTT_OK = 0, BUTT_TO_ERROR, BUTT_SHORT_ERROR
+  BUTT_OK = 0,
+  BUTT_TO_ERROR,
+  BUTT_SHORT_ERROR
 } ButtonError_t;
 
-#define _BUT_MAX_SCAN_PER   200
-#define _BUT_MIN_SCAN_PER   10
+#define _BUT_MAX_SCAN_PER 200
+#define _BUT_MIN_SCAN_PER 10
 
-#define _PDIR_OFFSET        (((unsigned int)&IODIR0 - (unsigned int)&IOPIN0)/sizeof(unsigned int))
-#define _PSET_OFFSET        (((unsigned int)&IOSET0 - (unsigned int)&IOPIN0)/sizeof(unsigned int))
-#define _PCLR_OFFSET        (((unsigned int)&IOCLR0 - (unsigned int)&IOPIN0)/sizeof(unsigned int))
-#define _PIN_OFFSET         (((unsigned int)&IOPIN0 - (unsigned int)&IOPIN0)/sizeof(unsigned int))
+#define _PDIR_OFFSET (((unsigned int)&IODIR0 - (unsigned int)&IOPIN0) / sizeof(unsigned int))
+#define _PSET_OFFSET (((unsigned int)&IOSET0 - (unsigned int)&IOPIN0) / sizeof(unsigned int))
+#define _PCLR_OFFSET (((unsigned int)&IOCLR0 - (unsigned int)&IOPIN0) / sizeof(unsigned int))
+#define _PIN_OFFSET (((unsigned int)&IOPIN0 - (unsigned int)&IOPIN0) / sizeof(unsigned int))
 
-#define _PORT1_BUT1_BIT     24
-#define _PORT1_BUT2_BIT     25
+#define _PORT1_BUT1_BIT 24
+#define _PORT1_BUT2_BIT 25
 
 //#define OS_DI() do {VICIntSelect &= ~0x10;} while(0)
 //#define OS_EI() do {VICIntSelect |= 0x10; VICIntEnable = VICIntEnable | 0x10;} while(0)
@@ -164,8 +170,8 @@ typedef enum _ButtonError_t
 
 typedef struct _ButtonCtrl_t
 {
-  volatile unsigned long * pButBaseReg;
-           unsigned int ButBit;
+  volatile unsigned long *pButBaseReg;
+  unsigned int ButBit;
 } ButtonCtrl_t, *pButtonCtrl_t;
 
 typedef struct _ButtonsPairCtrl_t
@@ -175,23 +181,19 @@ typedef struct _ButtonsPairCtrl_t
 } ButtonsPairCtrl_t, *pButtonsPairCtrl_t;
 
 const ButtonsPairCtrl_t ButtonsCtrl[1] =
-{
-  {
-    .Ba=
     {
-      .pButBaseReg = &IOPIN1,   // CAP_BUTT_1 = P1.24
-      .ButBit = _PORT1_BUT1_BIT,
-    },
-    .Bb=
-    {
-      .pButBaseReg = &IOPIN1,   // CAP_BUTT_2 = P1.25
-      .ButBit = _PORT1_BUT2_BIT,
-    }
-  }
-};
+        {.Ba =
+             {
+                 .pButBaseReg = &IOPIN1, // CAP_BUTT_1 = P1.24
+                 .ButBit = _PORT1_BUT1_BIT,
+             },
+         .Bb =
+             {
+                 .pButBaseReg = &IOPIN1, // CAP_BUTT_2 = P1.25
+                 .ButBit = _PORT1_BUT2_BIT,
+             }}};
 
-tU8
-readTouch(tU8 id, tU32 *pCount)
+tU8 readTouch(tU8 id, tU32 *pCount)
 {
   volatile unsigned int To;
   unsigned int Count, Hold, MasterMask, SlaveMask;
@@ -200,19 +202,19 @@ readTouch(tU8 id, tU32 *pCount)
 
   To = _BUT_MAX_SCAN_PER;
 
-  if(id & 1)
+  if (id & 1)
   {
-    pMasterReg = ButtonsCtrl[id>>1].Ba.pButBaseReg;
-    MasterMask = 1UL << ButtonsCtrl[id>>1].Ba.ButBit;
-    pSlaveReg  = ButtonsCtrl[id>>1].Bb.pButBaseReg;
-    SlaveMask  = 1UL << ButtonsCtrl[id>>1].Bb.ButBit;
+    pMasterReg = ButtonsCtrl[id >> 1].Ba.pButBaseReg;
+    MasterMask = 1UL << ButtonsCtrl[id >> 1].Ba.ButBit;
+    pSlaveReg = ButtonsCtrl[id >> 1].Bb.pButBaseReg;
+    SlaveMask = 1UL << ButtonsCtrl[id >> 1].Bb.ButBit;
   }
   else
   {
-    pMasterReg = ButtonsCtrl[id>>1].Bb.pButBaseReg;
-    MasterMask = 1UL << ButtonsCtrl[id>>1].Bb.ButBit;
-    pSlaveReg  = ButtonsCtrl[id>>1].Ba.pButBaseReg;
-    SlaveMask  = 1UL << ButtonsCtrl[id>>1].Ba.ButBit;
+    pMasterReg = ButtonsCtrl[id >> 1].Bb.pButBaseReg;
+    MasterMask = 1UL << ButtonsCtrl[id >> 1].Bb.ButBit;
+    pSlaveReg = ButtonsCtrl[id >> 1].Ba.pButBaseReg;
+    SlaveMask = 1UL << ButtonsCtrl[id >> 1].Ba.ButBit;
   }
 #if 0
   // Button scan algorithm
@@ -248,12 +250,12 @@ readTouch(tU8 id, tU32 *pCount)
 #endif
 
   // 5. Ba o0
-  *(pSlaveReg + _PCLR_OFFSET)  = SlaveMask;
+  *(pSlaveReg + _PCLR_OFFSET) = SlaveMask;
   *(pSlaveReg + _PDIR_OFFSET) |= SlaveMask;
 
   // 6. Set Ba i
   *(pSlaveReg + _PDIR_OFFSET) &= ~SlaveMask;
-//printf(", S=%d, ", IOPIN1 & (1UL<<25));
+  // printf(", S=%d, ", IOPIN1 & (1UL<<25));
 
   // 7. Set Bb o1
   OS_DI();
@@ -262,65 +264,65 @@ readTouch(tU8 id, tU32 *pCount)
   *(pMasterReg + _PSET_OFFSET) = MasterMask;
 
   // 8. wait and counting until Ba state get 1
-  while(!(*(pSlaveReg + _PIN_OFFSET) & SlaveMask))
+  while (!(*(pSlaveReg + _PIN_OFFSET) & SlaveMask))
   {
-    if(!To)
+    if (!To)
     {
       break;
     }
     --To;
   }
-//  Hold += T1TC - Count;
+  //  Hold += T1TC - Count;
   Hold = T1TC - Count;
   OS_EI();
-//printf(", S=%d; %d (%d) ", IOPIN1 & (1UL<<25), Hold, To);
-//printf("; %d (%d) ", Hold, To);
-  T1TCR = 0;  // disable Timer
+  // printf(", S=%d; %d (%d) ", IOPIN1 & (1UL<<25), Hold, To);
+  // printf("; %d (%d) ", Hold, To);
+  T1TCR = 0; // disable Timer
 
   // 9. Set Ba o1
-  *(pSlaveReg + _PSET_OFFSET)  = SlaveMask;
+  *(pSlaveReg + _PSET_OFFSET) = SlaveMask;
   *(pSlaveReg + _PDIR_OFFSET) |= SlaveMask;
 
-//  if (id == 0)
-// printf("\n");
-//else
-// printf("\n                          ");
-  if(!To)
+  //  if (id == 0)
+  // printf("\n");
+  // else
+  // printf("\n                          ");
+  if (!To)
   {
-//printf("BUTT_TO_ERROR");
-    return(BUTT_TO_ERROR);
+    // printf("BUTT_TO_ERROR");
+    return (BUTT_TO_ERROR);
   }
 
-  if(To == _BUT_MAX_SCAN_PER)
+  if (To == _BUT_MAX_SCAN_PER)
   {
-//printf("BUTT_SHORT_ERROR");
-    return(BUTT_SHORT_ERROR);
+    // printf("BUTT_SHORT_ERROR");
+    return (BUTT_SHORT_ERROR);
   }
 
   *pCount = Hold;
-//printf("BUTT_OK (%d)", Hold);
-  return(BUTT_OK);
+  // printf("BUTT_OK (%d)", Hold);
+  return (BUTT_OK);
 }
 
 /*****************************************************************************
  *
  * Description:
- *    A process entry function 
+ *    A process entry function
  *
  * Params:
- *    [in] arg - This parameter is not used in this application. 
+ *    [in] arg - This parameter is not used in this application.
  *
  ****************************************************************************/
 static void
-proc2(void* arg)
+proc2(void *arg)
 {
   tU8 pca9532Present = FALSE;
-  
+
   osSleep(50);
-  
-  //check if connection with PCA9532
+
+  // check if connection with PCA9532
   pca9532Present = pca9532Init();
-  
+
   if (TRUE == pca9532Present)
   {
     lcdInit();
@@ -329,164 +331,183 @@ proc2(void* arg)
 
     initKeyProc();
 
-	char bufferX[10];
-	char bufferY[10];
-
-    IODIR |= (1<<13)|(1<<14);
-    IOCLR = (1<<13)|(1<<14);
-
-    tU16 refXvalue = getAnalogueInput1(ACCEL_X);
-    tU16 refYvalue = getAnalogueInput1(ACCEL_Y);
-
-    lcdClrscr();
-    int counter = 0;
-    for(;;) {
-        tU8 keyPressed;
-        keyPressed = checkKey();
-
-//        if(keyPressed != KEY_NOTHING) {
-//            if (keyPressed == KEY_UP)
-//            {
-//                //playWithTheBall();
-//            }
-//        }
-
-    	tU16 inputX = getAnalogueInput1(ACCEL_X);
-    	tU16 inputY = getAnalogueInput1(ACCEL_Y);
-
-    	lcdPuts(bufferY);
-
-    	int valueX = refXvalue - inputX;
-    	int valueY = refYvalue - inputY;
-
-    	sprintf(bufferX,"%d",valueX);
-    	sprintf(bufferY,"%d",valueY);
-
-        lcdClrscr();
-    	lcdGotoxy(20,16);
-    	lcdPuts("X = ");
-    	lcdGotoxy(48,16);
-    	lcdPuts(bufferX);
-    	lcdGotoxy(12,30);
-    	lcdPuts("Y = ");
-    	lcdGotoxy(40,30);
-    	lcdPuts(bufferY);
-
-    	osSleep(100);
-    	counter = (counter + 1) % 10;
-    }
-
+    accControllProc();
   }
 }
 
 static void
-drawWelcome(void) {
-    lcdColor(0xff,0x00);
-    lcdClrscr();
-    lcdGotoxy(20,16);
-    lcdPuts("Welcome to");
-    lcdGotoxy(12,30);
-    lcdPuts("Ball The Game");
-    lcdGotoxy(58,64);
-    lcdPuts(":)");
-    lcdGotoxy(33,98);
-    lcdPuts("(C) 2022");
-    lcdGotoxy(32,112);
-    lcdPuts("(0.0.XDv)");
+drawWelcome(void)
+{
+  lcdColor(0xff, 0x00);
+  lcdClrscr();
+  lcdGotoxy(20, 16);
+  lcdPuts("Welcome to");
+  lcdGotoxy(12, 30);
+  lcdPuts("Ball The Game");
+  lcdGotoxy(58, 64);
+  lcdPuts(":)");
+  lcdGotoxy(33, 98);
+  lcdPuts("(C) 2022");
+  lcdGotoxy(32, 112);
+  lcdPuts("(0.0.XDv)");
 }
 
-static void 
-overdrawBall(struct Ball* ball, int color)
+static void
+overdrawBall(struct Ball *ball, int color)
 {
   lcdRect(ball->xPos, ball->yPos, ball->radius, ball->radius, color);
 }
 
 static void
-moveBall(struct Ball* ball, tU8 dir)
+moveBall(struct Ball *ball, tU8 dir)
 {
   overdrawBall(ball, 0x00);
   switch (dir)
   {
-      case KEY_UP:
-          ball->yPos = ball->yPos - ball->speed;
-          ball->yPos = abs(ball->yPos % lcdHeight);
-          break;
-      case KEY_DOWN:
-          ball->yPos = ball->yPos + ball->speed;
-          ball->yPos = abs(ball->yPos % lcdHeight);
-          break;
-      case KEY_LEFT:
-          ball->xPos = ball->xPos - ball->speed;
-          ball->xPos = abs(ball->xPos % lcdWidth);
-          break;
-      case KEY_RIGHT:
-          ball->xPos = ball->xPos + ball->speed;
-          ball->xPos = abs(ball->xPos % lcdWidth);
-          break;
-      default:
-          break;
+  case KEY_UP:
+    ball->yPos = ball->yPos - ball->speed;
+    ball->yPos = abs(ball->yPos % lcdHeight);
+    break;
+  case KEY_DOWN:
+    ball->yPos = ball->yPos + ball->speed;
+    ball->yPos = abs(ball->yPos % lcdHeight);
+    break;
+  case KEY_LEFT:
+    ball->xPos = ball->xPos - ball->speed;
+    ball->xPos = abs(ball->xPos % lcdWidth);
+    break;
+  case KEY_RIGHT:
+    ball->xPos = ball->xPos + ball->speed;
+    ball->xPos = abs(ball->xPos % lcdWidth);
+    break;
+  default:
+    break;
   }
   overdrawBall(ball, 0xFF);
 }
 
 static void
-playWithTheBall(void) {
-    struct Ball ball;
-    ball.xPos = 10;
-    ball.yPos = 10;
-    ball.speed = 5;
-    ball.radius = 4;
+accControlX(struct Ball *ball, tU16 refValue)
+{
+  tU32 delay;
+  tU16 input, value, strength;
+  tU8 dir;
 
-    lcdClrscr();
-    lcdColor(0x00, 0xFF);
-    lcdRect(ball.xPos, ball.yPos, ball.radius, ball.radius, 0xFF);
+  for(;;)
+  {
+    input = getAnalogueInput1(ACCEL_X);
+    value - refValue - input;
 
-    tU8 keypress;
-
-    lcdClrscr();
-
-    for(;;) {
-        keypress = checkKey();
-        if (keypress != KEY_NOTHING)
-        {
-            if (keypress == KEY_CENTER) {
-                direction = KEY_NOTHING;
-                break;
-            }
-          
-            if ((keypress == KEY_UP)    ||
-                (keypress == KEY_RIGHT) ||
-                (keypress == KEY_DOWN)  ||
-                (keypress == KEY_LEFT))
-            direction = keypress;
-        }
-
-        if (direction != KEY_NOTHING)
-        {
-            moveBall(&ball, direction);
-            direction = KEY_NOTHING;
-        }  
+    if (value > 0)
+    {
+      dir = KEY_UP;
     }
+    else 
+    {
+      dir = KEY_DOWN;
+    }
+
+    strength = abs(value / 20);
+    if (strength == 0)
+    {
+      delay = 10;
+    }
+    else
+    {
+      if (strength > 5)
+      {
+        strength = 5;
+      }
+      delay = 100 - 10 * strength;
+    }
+    moveBall(ball, dir);
+    osSleep(delay);
+  }
+}
+
+static void
+accControllProc()
+{
+  struct Ball ball;
+  ball.xPos = 10;
+  ball.yPos = 10;
+  ball.speed = 5;
+  ball.radius = 4;
+
+  lcdClrscr();
+  lcdColor(0x00, 0xFF);
+  lcdRect(ball.xPos, ball.yPos, ball.radius, ball.radius, 0xFF);
+
+  IODIR |= (1 << 13) | (1 << 14);
+  IOCLR = (1 << 13) | (1 << 14);
+
+  tU16 refXvalue = getAnalogueInput1(ACCEL_X);
+  tU16 refYvalue = getAnalogueInput1(ACCEL_Y);
+  
+  accControlX(&ball, refXvalue);
+}
+
+static void
+playWithTheBall(void)
+{
+  struct Ball ball;
+  ball.xPos = 10;
+  ball.yPos = 10;
+  ball.speed = 5;
+  ball.radius = 4;
+
+  lcdClrscr();
+  lcdColor(0x00, 0xFF);
+  lcdRect(ball.xPos, ball.yPos, ball.radius, ball.radius, 0xFF);
+
+  tU8 keypress;
+
+  lcdClrscr();
+
+  for (;;)
+  {
+    keypress = checkKey();
+    if (keypress != KEY_NOTHING)
+    {
+      if (keypress == KEY_CENTER)
+      {
+        direction = KEY_NOTHING;
+        break;
+      }
+
+      if ((keypress == KEY_UP) ||
+          (keypress == KEY_RIGHT) ||
+          (keypress == KEY_DOWN) ||
+          (keypress == KEY_LEFT))
+        direction = keypress;
+    }
+
+    if (direction != KEY_NOTHING)
+    {
+      moveBall(&ball, direction);
+      direction = KEY_NOTHING;
+    }
+  }
 }
 
 /*****************************************************************************
  *
  * Description:
- *    The entry function for the initialization process. 
+ *    The entry function for the initialization process.
  *
  * Params:
- *    [in] arg - This parameter is not used in this application. 
+ *    [in] arg - This parameter is not used in this application.
  *
  ****************************************************************************/
 static void
-initProc(void* arg)
+initProc(void *arg)
 {
   tU8 error;
 
-  eaInit();   //initialize printf
-  i2cInit();  //initialize I2C
-//  osCreateProcess(proc1, proc1Stack, PROC1_STACK_SIZE, &pid1, 3, NULL, &error);
-//  osStartProcess(pid1, &error);
+  eaInit();  // initialize printf
+  i2cInit(); // initialize I2C
+  //  osCreateProcess(proc1, proc1Stack, PROC1_STACK_SIZE, &pid1, 3, NULL, &error);
+  //  osStartProcess(pid1, &error);
   osCreateProcess(proc2, proc2Stack, PROC2_STACK_SIZE, &pid2, 3, NULL, &error);
   osStartProcess(pid2, &error);
 
@@ -505,8 +526,7 @@ initProc(void* arg)
  *    [in] elapsedTime - The number of elapsed milliseconds since last call.
  *
  ****************************************************************************/
-void
-appTick(tU32 elapsedTime)
+void appTick(tU32 elapsedTime)
 {
   msClock += elapsedTime;
 }
